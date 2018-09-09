b9071f91aef0aa893236ff5aa78cdcb5d73e4704
11000783 <josephaguilar2022@gmail.com>
2018-09-09 14:26:48 -0700

    textundoredo ip

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -23,16 +23,12 @@ public class _02_TextUndoRedo implements KeyListener {
 	 * */
 	JLabel label;
 	Stack<Character> deleted;
-	Stack<Character> chars;
-	String text;
 	
 	public _02_TextUndoRedo() {
 		deleted = new Stack<>();
-		chars = new Stack<>();
-		text = "";
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
-		label = new JLabel(text);
+		label = new JLabel("");
 		panel.add(label);
 		frame.add(panel);
 		frame.addKeyListener(this);
@@ -44,10 +40,8 @@ public class _02_TextUndoRedo implements KeyListener {
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		chars.push(e.getKeyChar());
-		text = text + e.getKeyChar();
-		label.setText(text);
+		// TODO Auto-generated method stu
+		label.setText(label.getText() + e.getKeyChar());
 	}
 
 	@Override
@@ -59,10 +53,19 @@ public class _02_TextUndoRedo implements KeyListener {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getKeyCode() == 8) {
-			
+		if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+		  deleted.push('a');
+		 label.setText(label.getText().substring(0, label.getText().length()-2));
 		}
+
+
+		if(e.getKeyCode() == KeyEvent.VK_SHIFT) {
+			System.out.println(deleted.size());
+			Character s = deleted.pop();
+			System.out.println(s);
+			label.setText(label.getText() + Character.toString(s));
+		
 		
 	}
-	
+	}
 }
