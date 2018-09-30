39d39da242e41bb46c42e0c49dcf728ce19de610
AnkitBisain <league@WTS10.attlocal.net>
2018-09-30 14:26:27 -0700

    Finished HangMan, StringInteger pair IP

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,25 +1,99 @@
 package _04_HangMan;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Stack;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import _04_HangMan.Utilities;
 
-public class HangMan{
+public class HangMan implements KeyListener{
+	
+	JLabel label;
+	JPanel panel;
+	JFrame frame;
+	int lives;
+	String word;
+	String s;
+	Stack<String> words;
 	
 	public HangMan() {
 		int numbOfWords = Integer.parseInt(JOptionPane.showInputDialog("Welcome. How many rounds would you like to play (must be an integer between 1 and 256)?"));
-		Stack<String> words = new Stack<String>();
+		words = new Stack<String>();
 		for(int i = 0; i < numbOfWords; i ++) {
-			String s = Utilities.readRandomLineFromFile("dictionary.txt");
-			words.push(s);
+			String q = "yeet";
+			words.push(q);
 		}
+		lives = 10;
+		word = words.pop();
+		label = new JLabel();
+		panel = new JPanel();
+		frame = new JFrame();
+		s = "";
+		for(int i = 0; i < word.length(); i++) {
+			s = s + "_";
+		}
+		label.setText(s + "    Lives Remaining: " + lives);
+		panel.add(label);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+		frame.addKeyListener(this);
 	
 	}
 	
 	public static void main(String[] args) {
+		new HangMan();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		String t = s;
+		Character c = e.getKeyChar();
+		for(int i = 0; i < word.length(); i++){
+				if(word.charAt(i) == c) {
+					s = s.substring(0, i) + c + s.substring(i+1,s.length());
+				}
+			}
+		if(s == t) {
+			lives --;
+		}
+		label.setText(s + "    Lives Remaining: " + lives);
+		if(lives == 0) {
+			JOptionPane.showMessageDialog(null, "how are you so bad");
+		}
+		System.out.println(word + "----" + s );
+		if(word.equals(s)) {
+			JOptionPane.showMessageDialog(null, "how are you so good");
+		}	
+		if(lives == 0 || word.equals(s)) {
+			lives = 10;
+			word = words.pop();
+			s = "";
+			for(int i = 0; i < word.length(); i++) {
+				s = s + "_";
+			}
+			
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 	
 }
